335ad34268e0c0cc5e0bdec293f3a318639f557d
James Hawk <jph94880@gmail.com>
2024-03-13 23:08:58 -0500

    Adds switch to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
 import NavBar from "./NavBar";
-
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import { Component } from 'react'
 
 import "./App.css";
 
 function App() {
+  console.log(window.location.pathname)
+  
+  switch(window.location.pathname) {
+    case "/":
+    Component = Home 
+    break;
+    case "/about":
+    Component = About
+    break;
+    case "/pricing":
+    Component = Pricing
+    break;
+
+  }
+
 
   return (
-<>
-<NavBar/>
-</>
+    <>
+      <NavBar />
+      <Component/>
+    </>
   );
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./styles.css"
 
+function customLink(e) {
+  e.preventDefault()
+  console.log("Start the lecture at 37:21. We are going to create a custom component. https://vimeo.com/866475274/19288d9c6d?share=copy")
+}
+
 const NavBar = () => {
+  const path = window.location.pathname;
+
+
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
+      <a href="/" className="site-title" onClick={(e) => customLink(e)}>
         Business Site
       </a>
       <ul>
-        <li>
+        <li className="active">
           <a href='/pricing'>Pricing</a>
         </li>
         <li>
