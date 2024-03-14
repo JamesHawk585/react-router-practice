e739ce5af8a593886d788dc56c6d84fa569b1fbf
James Hawk <jph94880@gmail.com>
2024-03-14 16:29:31 -0500

    Adds custom routing without using an external library

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-import "./styles.css"
+import "./styles.css";
 
-function customLink(e) {
-  e.preventDefault()
-  console.log("Start the lecture at 37:21. We are going to create a custom component. https://vimeo.com/866475274/19288d9c6d?share=copy")
+function CustomLink({href, children, ...props}) {
+  const path = window.location.pathname;
+  return(
+    <li className={path === href ? "active" : ""}>
+      <a href={href} {...props}>{children}</a>
+    </li>
+  );
 }
 
 const NavBar = () => {
   const path = window.location.pathname;
 
-
   return (
     <nav className="nav">
-      <a href="/" className="site-title" onClick={(e) => customLink(e)}>
-        Business Site
-      </a>
+      <a href="/" className="site-title">Business Site</a>
       <ul>
-        <li className="active">
-          <a href='/pricing'>Pricing</a>
-        </li>
-        <li>
-        <a href='/about'>About</a>
-        </li>
+        <CustomLink href="/about">About</CustomLink>
+        <CustomLink href="/pricing">Pricing</CustomLink>
       </ul>
     </nav>
   );
